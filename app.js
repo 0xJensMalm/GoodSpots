b8136bcf009e7dadbdfc7d110d4a4cc272144306
0xJensMalm <112858577+0xJensMalm@users.noreply.github.com>
2024-05-27 08:08:20 +0200

    left toolbar

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ import { customIcon, createCustomMarker } from "./utils.js";
 import { map, initializeMap } from "./map.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Add event listener to the locate button
-  document
-    .getElementById("locate-button")
-    .addEventListener("click", function () {
-      console.log("Locate Me button clicked");
-      moveToUserLocation();
-    });
-
   // Initialize the map at a default location (if needed)
   console.log("Initializing map with default location.");
   initializeMap(51.505, -0.09);
@@ -29,6 +21,42 @@ document.addEventListener("DOMContentLoaded", function () {
     [51.51, -0.12],
   ];
   var polyline = L.polyline(latlngs, { color: "blue" }).addTo(map);
+
+  // Add geocoder control to the map
+  var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false,
+  })
+    .on("markgeocode", function (e) {
+      var center = e.geocode.center;
+      map.setView(center, map.getZoom());
+      if (currentMarker) {
+        map.removeLayer(currentMarker);
+      }
+      currentMarker = L.marker(center, { icon: customIcon })
+        .addTo(map)
+        .bindPopup(e.geocode.name)
+        .openPopup();
+    })
+    .addTo(map);
+
+  // Add event listener to the locate button
+  document
+    .getElementById("locate-button")
+    .addEventListener("click", function () {
+      console.log("Locate Me button clicked");
+      moveToUserLocation();
+    });
+
+  // Add event listener to the search button
+  document
+    .getElementById("search-button")
+    .addEventListener("click", function () {
+      console.log("Search button clicked");
+      var searchBox = document.querySelector(".leaflet-control-geocoder");
+      if (searchBox) {
+        searchBox.querySelector("input").focus();
+      }
+    });
 
   // Add interactivity to add custom markers on click
   map.on("click", function (e) {
